4efb571f353106fc27d29bde2eb488dc5aefcad6
Valeria <krasovskayameow@gmail.com>
2022-11-10 11:11:02 +0300

    Добавила каркас кода

--- controlnaya/Program.cs
+++ controlnaya/Program.cs
@@ -1,7 +1,21 @@
 ﻿
 
+void SortMassive(string[] FirstMassive)
+{
+    int j = 0;
+    string[] ResultMassive = new string[j];
+    {
+        for(int i = 0; i < FirstMassive.Length; i++)
+        {
+            if(FirstMassive[i].Length < 4)
+            Console.WriteLine(FirstMassive[i]);
+        }
+    }
+}
 
 Console.Clear();
-Console.WriteLine("Введите количество элементов массива");
-int n = Convert.ToInt32(Console.ReadLine());
-string[] Words = new string[n];
+string[] FirstMassive = { "Moscow", "sun", "1703" };
+Console.Write($"Исходный массив: [{string.Join(",",FirstMassive)}]");
+Console.WriteLine();
+Console.WriteLine("Результат: ");
+SortMassive(FirstMassive);
