9a1521c6443291d0723552630de142974d93c18c
Valeria <krasovskayameow@gmail.com>
2022-11-10 11:32:16 +0300

    Добавила элементы массива

--- controlnaya/Program.cs
+++ controlnaya/Program.cs
@@ -1,21 +1,19 @@
 ﻿
 void SortMassive(string[] FirstMassive)
-{
-    int j = 0;
-    string[] ResultMassive = new string[j];
+
     {
         for(int i = 0; i < FirstMassive.Length; i++)
         {
             if(FirstMassive[i].Length < 4)
             Console.WriteLine(FirstMassive[i]);
-        }
+        };
     }
-}
+
 
 
 Console.Clear();
-string[] FirstMassive = { "Moscow", "sun", "1703" };
+string[] FirstMassive = { "Moscow", "sun", "1703", "<33", "NY" };
 Console.Write($"Исходный массив: [{string.Join(",",FirstMassive)}]");
 Console.WriteLine();
-Console.WriteLine("Результат: ");
+Console.Write("Результат: ");
 SortMassive(FirstMassive);
