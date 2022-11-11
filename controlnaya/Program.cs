52588f513d48aa6c9ed38c2c633cfdc9170ad4f7
Valeria <krasovskayameow@gmail.com>
2022-11-11 16:48:58 +0300

    Последние правки

--- controlnaya/Program.cs
+++ controlnaya/Program.cs
@@ -5,8 +5,10 @@ void SortMassive(string[] FirstMassive)
         for(int i = 0; i < FirstMassive.Length; i++)
         {
             if(FirstMassive[i].Length < 4)
-            Console.WriteLine(FirstMassive[i]);
-        }
+            {
+            Console.Write(FirstMassive[i] + " ");
+            }
+        } 
     }
 
 
@@ -15,5 +17,6 @@ Console.Clear();
 string[] FirstMassive = { "Moscow", "sun", "1703", "<33", "NY" };
 Console.Write($"Исходный массив: [{string.Join(",",FirstMassive)}]");
 Console.WriteLine();
-Console.Write("Результат: ");
+Console.Write("Результат: [");
 SortMassive(FirstMassive);
+Console.Write("]");
